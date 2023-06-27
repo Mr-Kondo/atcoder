aaaaac262e1d60f82546f3338555d6086892c977
Mr. KONDO <power-is-justice@hotmail.co.jp>
2023-06-27 11:37:27 +0900

    Add files via upload

--- abc/085_c.cpp
+++ abc/085_c.cpp
@@ -13,12 +13,10 @@ int main() {
   }
   for(int i=0; i<=N; i++){
   	for(int j=0; j<=N-i; j++){
-    	for(int k=0; k<=N-i-j; k++){
-        	if((10*i+5*j+k == Y/1000)&&(i+j+k == N)){
-            	cout << i << " " << j << " " << k << endl;
+        	if(10*i+5*j+(N-i-j) == Y/1000){
+            	cout << i << " " << j << " " << (N-i-j) << endl;
               	flag = true;
               	return 0;
-            }
         }
     }
   }
