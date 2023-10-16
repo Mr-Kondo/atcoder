41f66a9ef75a16108cfda8f82aa5423862e28573
Mr. KONDO <power-is-justice@hotmail.co.jp>
2023-10-17 05:40:58 +0900

    Add files via upload

--- abc/153_a.cpp
+++ abc/153_a.cpp
@@ -4,11 +4,7 @@ using namespace std;
 int main() {
   int H, A;
   cin >> H >> A;
-  if( H <= A ) { cout << 1 << endl;}
-  else{
-    int Ans = H/A;
-    if( A*Ans >= H){cout << Ans << endl;}
-    else{ cout << Ans+1 << endl;}
-  }
+  if( H%A == 0 ) { cout << H/A << endl;}
+  else{ cout << H/A +1 << endl;}
   return 0;
 }
