fa35fefd85f78a62ae6d67c04777048cc3b175a8
Mr. KONDO <power-is-justice@hotmail.co.jp>
2023-11-01 06:54:13 +0900

    Add files via upload

--- abc/088_a.cpp
+++ abc/088_a.cpp
@@ -1,15 +1,18 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
 int main() {
-  int N = 0, A = 0;
-  cin >> N;
-  cin >> A;
-  if( N - N/500*500 <= A )
-  {
-    cout << "Yes";
-  }else
-  {
-    cout << "No";
-  } 
+    int N, A;
+    cin >> N;
+    cin >> A;
+
+    int num = 0;
+    num = N / 500;
+
+    if(A >= N-num*500){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+
+    return 0;
 }
