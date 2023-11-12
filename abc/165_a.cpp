38721b585488656d9b97b44606151920edbbd5ab
Mr. KONDO <power-is-justice@hotmail.co.jp>
2023-11-12 11:30:09 +0900

    Add files via upload

--- abc/165_a.cpp
+++ abc/165_a.cpp
@@ -5,16 +5,11 @@ int main() {
     int K, A, B;
     cin >> K;
     cin >> A >> B;
-    int div_num = A/K;
-    int i_num = div_num*K;
     bool flg = false;
-    for(int i=i_num; i<B+1; i++){
-        if(A<=i){
-            if(i%K == 0){flg=true;}
-        }
+    for(int i=A; i<=B; ++i){
+        if(i%K == 0){flg=true;}
     }
-
-    if(flg == true){cout << "OK" << endl;}
+    if(flg){cout << "OK" << endl;}
     else {cout << "NG" << endl;}
     return 0;
 }
