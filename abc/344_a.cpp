537ddbb19d053c1bab699b2319c3f0f7fb08282a
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-03-10 12:57:34 +0900

    Update 344_a.cpp

--- abc/344_a.cpp
+++ abc/344_a.cpp
@@ -2,21 +2,10 @@
 using namespace std;
 
 int main() {
-  string str;
-  cin >> str;
-  short cnt_1 = -9999;
-  short cnt_2 = -9999;
-  for(int i=0; i<str.size();++i){
-    if(str[i] == '|'){
-      if(cnt_1 >= 0){
-        cnt_2 = i;
-        break;
-      }
-      cnt_1 = i;
-    }
-  }
   
- if(cnt_1 != 0) cout << str.substr(0, cnt_1) + str.substr(cnt_2+1, str.size()-1); 
- else cout << str.substr(cnt_2+1, str.size()-1);
+ string str;
+ cin >> str;
+ cout << str.substr(0, str.find('|', 0)) + str.substr(str.rfind( '|', str.size())+1, str.size());
+   
  return 0;
 }
