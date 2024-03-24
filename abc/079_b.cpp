44a84164da14baa0425136a39c6ef227088f80ee
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-03-24 18:59:04 +0900

    Update 079_b.cpp

--- abc/079_b.cpp
+++ abc/079_b.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,n) for (int i=0; i<(n); ++i)
-using ll = long long;
+
 
 int main() {
   int N;
   cin >> N;
   
-  vector<ll> L(N+1);
+  vector<long long> L(N+1);
   L[0] = 2; L[1] = 1;
   
   for(int i=2; i<N+1; ++i) L[i] = L[i-2]+L[i-1];
