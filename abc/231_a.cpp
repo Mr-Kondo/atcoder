aafe7a4cb673efc7ec94d1afd48d3b115d0848e1
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-04-16 20:48:59 +0900

    Create 231_a.cpp

---
+++ abc/231_a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+int main(){
+  int D;
+  cin >> D;
+  cout << (float)D/100 << '\n';
+  return 0;
+}
