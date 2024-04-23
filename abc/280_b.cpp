cf6152017f9bec12f41587ba6b013a98dfc0aa97
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-04-23 20:40:38 +0900

    Update 280_b.cpp

--- abc/280_b.cpp
+++ abc/280_b.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
 
-int main() {
+int main(){
   int N;
   cin >> N;
+  vector<ll> S(N);
+  rep(i,N) cin >> S[i];
   
-  vector<long long> vec;
-  for(int i=0; i<N; ++i){
-    int tmp;
-    cin >> tmp;
-    vec.push_back(tmp);
+  vector<ll> A;
+  rep(i,N){
+    if(i==0) A.push_back(S[i]);
+    else A.push_back(S[i] - reduce(begin(A), end(A)));
   }
   
-  for(int i=0; i<N; ++i){
-    if(i == 0){ cout << vec[i]; continue;}
-    cout << " " + to_string(vec[i]-vec[i-1]);
-  }
-  
+  rep(i,N) cout << A[i] << ' ';
   return 0;
 }
