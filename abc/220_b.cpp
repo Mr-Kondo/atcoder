764bb2fa72d5cbd6f371079990c012d403ccdaf8
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-05-02 21:35:27 +0900

    Update 220_b.cpp

--- abc/220_b.cpp
+++ abc/220_b.cpp
@@ -1,44 +1,24 @@
-#include <iostream>
-#include <string>
-#include <cmath>
-
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
 
-int* split_digit_number(long long Num, int* Array, int size){
-    for(int i=0; i<size; i++){
-        long long digit = std::pow((double)10, (double)size-1-i);
-        Array[i] = Num / digit;
-        Num -= digit*Array[i];
+long long convert_to_base_K_num_to_decimal(long long N, int K){
+    long long return_num = 0;
+    int itr = 0;
+    while(N!=0){
+        return_num += N%10 * pow(K, itr);
+        N /= 10;
+        itr++;
     }
-    return Array;
-}
-
-int convert_base_K_num_to_decimal(long long N, int K){ 
-    // This function is to convert N in base-K to dicimal number.
-    long long decimal_num = 0;                // A return variable which is decimal(base in 10).
-
-    string str_N = to_string(N);        // A string type variable which used to calculate N's digit by string size.
-    int digit_num = str_N.size();       // A integer type variable which is N'digit.
-    int* digit_N = new int[digit_num];  // An array of integer type variable, whose each element has digit number of N. 
-
-    split_digit_number(N, digit_N, digit_num);
-
-    for(int i = 0; i < digit_num; i++){
-        decimal_num += pow(K,digit_num-1 - i) * digit_N[i];
-    } 
-
-    return decimal_num;
+    return return_num;
 }
 
 int main(){
-    int K;
-    long long A, B;
-    long long Ans = 0;
-    cin >> K;
-    cin >> A >> B;
-    A = convert_base_K_num_to_decimal(A, K);
-    B = convert_base_K_num_to_decimal(B, K);
-    Ans = A * B;
-    cout << Ans << endl;
-    return 0;
+  int K;
+  cin >> K;
+  ll A, B;
+  cin >> A >> B;
+  cout << convert_to_base_K_num_to_decimal(A, K)*convert_to_base_K_num_to_decimal(B, K) << '\n';
+  return 0;
 }
