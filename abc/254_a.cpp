4310ca1269b5471982687a5d2a5ddc1d0f39ce1e
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-05-02 21:32:53 +0900

    Create 254_a.cpp

---
+++ abc/254_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+int main(){
+  string N;
+  cin >> N;
+  cout << N[N.size()-2] << N[N.size()-1] << '\n';
+
+  return 0;
+}
