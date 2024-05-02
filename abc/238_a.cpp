45fbf7d08711762693890cd359ce2272c56da861
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-05-02 22:12:42 +0900

    Update 238_a.cpp

--- abc/238_a.cpp
+++ abc/238_a.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
 
 int main() {
     unsigned int n = 0;
