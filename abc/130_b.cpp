e72a6d75354d775e3ace32cfb17912f34f5bea0e
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-05-03 20:17:04 +0900

    Create 130_b.cpp

---
+++ abc/130_b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+
+int main()
+{
+  int N, X;
+  cin >> N >> X;
+  
+  int L = 0;
+  int D = 0;
+  int ans = 1;
+  
+  rep(i,N){
+    cin >> L;
+    D += L;
+    if(X < D){
+      break;
+    }
+    ans++;
+  }
+  
+  cout << ans << '\n';
+  return 0;
+}
