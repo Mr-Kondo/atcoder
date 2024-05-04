837d44b2fbbc715dcb4198e02041ab06ef859727
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-05-04 18:44:14 +0900

    Create 007_a.cpp

---
+++ abc/007_a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+int main()
+{
+  int N;
+  cin >> N;
+  cout << N-1 << '\n';
+  
+  return 0;
+}
