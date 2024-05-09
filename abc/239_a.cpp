cfbad9083ad81eba73ba4ba8f64b0b7c93f2ffdd
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-05-09 21:22:27 +0900

    Update 239_a.cpp

--- abc/239_a.cpp
+++ abc/239_a.cpp
@@ -3,8 +3,6 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using ll = long long;
 
-// Ref "https://www.youtube.com/watch?v=p92To1h16-k&t=872s"
-
 int main()
 {
   ll H;
