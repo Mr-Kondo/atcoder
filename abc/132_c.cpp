3803bd646ab78efc872ff475e592f39ed23d5481
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-05-13 21:28:53 +0900

    Create 132_c.cpp

---
+++ abc/132_c.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+// Reffernce Qiita,"Vectorの平均値と中央値を求める", https://qiita.com/kajitack/items/aaf4721c55e372de3c20"
+
+int main()
+{
+  int N;
+  cin >> N;
+  
+  vector<int> d(N);
+  rep(i,N) cin >> d[i];
+  
+  sort(d.begin(), d.end());
+  
+  int median = 0;
+  if(d.size()%2 == 1) median = d[N/2];
+  else                median = d[((N/2-1) + N/2) / 2];
+  
+  int median_next = 0;
+  if(d.size()%2 == 1) median_next = d[N/2 + 1];
+  else                median_next = d[((N/2-1) + N/2) / 2 + 1];
+  
+  
+  int ans = median_next - median;
+  
+  cout << ans << '\n';
+  return 0;
+}
