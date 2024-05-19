6856da793c54450ab50f0bc569937770080faa64
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-05-19 21:47:24 +0900

    Create 215_a.cpp

---
+++ abc/215_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+constexpr ll INF = 1<<30;
+
+int main()
+{
+  string str;
+  cin >> str;
+  if(str == "Hello,World!") cout << "AC" << '\n';
+  else                      cout << "WA" << '\n';
+  return 0;
+}
