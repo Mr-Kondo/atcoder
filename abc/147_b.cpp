0e403c189730efc8174a0498452c9ba96034a4b8
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-05-21 18:35:40 +0900

    Create 147_b.cpp

---
+++ abc/147_b.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+constexpr ll INF = 1<<30;
+
+int main()
+{
+  string S;
+  cin >> S;
+  
+  
+  string rS = S;
+  reverse(rS.begin(), rS.end());
+  
+  int ans = 0;
+  
+  rep(i,S.size()){
+    if(S[i] != rS[i]) ans++;
+  }
+  
+  cout << ans/2 <<'\n';
+  
+  return 0;
+}
