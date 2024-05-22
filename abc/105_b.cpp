789b5bd393b234368d4200e6d9557a7132d6d8f1
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-05-22 22:29:05 +0900

    Create 105_b.cpp

---
+++ abc/105_b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+constexpr ll INF = 1<<30;
+
+// Refference : アルゴ式, "約数列挙のアルゴリズム", "https://algo-method.com/descriptions/84"
+
+int main()
+{
+  int N;
+  cin >> N;
+  
+  int ans = 0;
+  rep(i,N){
+    int count = 1;
+    if((i+1)%2 == 1){
+      rep(j,i){
+        if((i+1)%(j+1) == 0){
+          count++;
+          if(count == 8){
+            ans++;
+            //cout << "i = " << i+1 << '\n';
+          }
+        }
+      }
+    }
+  }
+  
+  cout << ans << '\n';
+  return 0;
+}
+
