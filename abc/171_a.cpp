3e33ec970ab72f0c956ecc3289b3e3a9b5cea04c
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-05-27 15:33:33 +0900

    Create 171_a.cpp

---
+++ abc/171_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+constexpr ll INF = 1<<30;
+
+int main()
+{
+  char ch;
+  cin >> ch;
+  if(isupper(ch)) cout << 'A' << '\n';
+  else            cout << 'a' << '\n';
+  return 0;
+}
