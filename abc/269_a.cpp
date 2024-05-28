fb1811ddb3e0752761e6e524829ee7c81e479554
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-05-28 20:32:12 +0900

    Create 269_a.cpp

---
+++ abc/269_a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+constexpr ll INF = 1<<30;
+
+int main()
+{
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  cout << (a+b)*(c-d) << '\n';
+  cout << "Takahashi" << '\n';
+  return 0;
+}
