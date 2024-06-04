d524d51b80aff56cde55b4d17cf5e361ebf7b4df
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-06-04 21:43:23 +0900

    Create 001_a.cpp

---
+++ abc/001_a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+constexpr ll INF = 1<<30;
+
+int main()
+{
+  int H1, H2;
+  cin >> H1 >> H2;
+  cout << H1 - H2 << '\n';
+  return 0;
+}
