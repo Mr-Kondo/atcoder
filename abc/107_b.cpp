6a862a665be0292e8369a9794fbe283bb1431839
Mr. KONDO <power-is-justice@hotmail.co.jp>
2024-08-02 22:17:28 +0900

    Update 107_b.cpp

--- abc/107_b.cpp
+++ abc/107_b.cpp
@@ -5,72 +5,91 @@ using ll = long long;
 
 constexpr ll INF = 1<<30;
 
-vector<char> Input(int H, int W){
-    vector<char> a(H*W, '.');
-    for (int j = 0; j < H; j++){
-        for (int i = 0; i < W; i++){
-            cin >> a.at(j*W+i);
+
+vector<char> readGrid(int height, int width) {
+    vector<char> grid(height * width);
+    for (int row = 0; row < height; ++row) {
+        for (int col = 0; col < width; ++col) {
+            cin >> grid[row * width + col];
         }
     }
-    return a;
+    return grid;
 }
 
-vector<int> Search_Row(vector<char> a, int H, int W){
-    // Search row
-    bool flag;
-    vector<int> rows;
-    for (int j = 0; j < H; j++){
-        flag = false;
-        for (int i = 0; i < W; i++){
-            if (a.at(j*W+i) == '#'){
-                flag = true;
-                continue;
+vector<int> searchRowsWithObstacles(const vector<char>& grid, int height, int width) {
+    vector<int> rowIndices;
+
+    for (int row = 0; row < height; ++row) {
+        bool hasObstacle = false;
+
+        for (int column = 0; column < width; ++column) {
+            if (grid[row * width + column] == '#') {
+                hasObstacle = true;
+                break;
             }
         }
-        if (flag) rows.push_back(j);
+
+        if (hasObstacle) {
+            rowIndices.push_back(row);
+        }
     }
-    return rows;
+
+    return rowIndices;
 }
 
-vector<int> Search_Column(vector<char> a, int H, int W){
-    // Search column
-    bool flag;
-    vector<int> cols;
-    for (int i = 0; i < W; i++){
-        flag = false;
-        for (int j = 0; j < H; j++){
-            if (a.at(j*W+i) == '#'){
-                flag = true;
-                continue;
+vector<int> searchColumns(const vector<char>& grid, int height, int width) {
+    vector<int> columnIndices;
+
+    for (int column = 0; column < width; ++column) {
+        bool hasObstacle = false;
+
+        for (int row = 0; row < height; ++row) {
+            if (grid[row * width + column] == '#') {
+                hasObstacle = true;
+                break;
             }
         }
-        if (flag) cols.push_back(i);
+
+        if (hasObstacle) {
+            columnIndices.push_back(column);
+        }
     }
-    return cols;
+
+    return columnIndices;
 }
 
-void Display(vector<int> rows, vector<int> cols){
-    rep(i,rows.size()){cout << "H = " << rows.at(i)+1 << endl;}
-    rep(i,cols.size()){cout << "W = " << cols.at(i)+1 << endl;}
+void displayRowAndColumnIndices(const vector<int>& rowIndices, const vector<int>& columnIndices)
+{
+    for (const auto& rowIndex : rowIndices) {
+        cout << "H = " << rowIndex + 1 << endl;
+    }
+
+    for (const auto& columnIndex : columnIndices) {
+        cout << "W = " << columnIndex + 1 << endl;
+    }
 }
 
-void Output(vector<char> a, int H, int W, vector<int> rows, vector<int> cols){
-    for(int j = 0; j < rows.size(); j++){
-        for (int i = 0; i < cols.size(); i++){
-            cout <<  a.at(rows.at(j)*W+cols.at(i));
+void outputGrid(const vector<char>& grid, int height, int width, const vector<int>& rowIndices, const vector<int>& columnIndices)
+{
+    for (const auto& rowIndex : rowIndices) {
+        for (const auto& columnIndex : columnIndices) {
+            cout << grid[rowIndex * width + columnIndex];
         }
         cout << '\n';
     }
 }
+
 int main()
 {
-    int H, W;
-    cin >> H >> W;
-    vector<char> a = Input(H, W);
-    vector<int> rows = Search_Row(a, H, W);
-    vector<int> cols = Search_Column(a, H, W);
+    int height, width;
+    cin >> height >> width;
 
-    //Display(rows, cols);
-    Output(a, H, W, rows, cols);
+    vector<char> grid = readGrid(height, width);
+    vector<int> rowIndices = searchRowsWithObstacles(grid, height, width);
+    vector<int> columnIndices = searchColumns(grid, height, width);
+
+    //displayRowAndColumnIndices(rowIndices, columnIndices);
+    outputGrid(grid, height, width, rowIndices, columnIndices);
+
     return 0;
 }
