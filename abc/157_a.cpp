13d5bb9934bcf476f2fb163899b70461254b60fb
Masato MIWADA <power-is-justice@hotmail.co.jp>
2025-02-05 00:03:20 +0900

    Add files via upload

--- abc/157_a.cpp
+++ abc/157_a.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
-#include<math.h>
+// ABC 157 A - Duplex Printing
+// "https://atcoder.jp/contests/abc157/tasks/abc157_a"
+
+#include <bits/stdc++.h>
 using namespace std;
-int main() {
-    double N;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+constexpr ll INF = 1<<30;
+
+int main(int argc, char* argv[]){
+    int N;
     cin >> N;
-    cout << ceil(N/2) << endl;
-    return 0;
+    // Since each sheet can hold 2 pages, the number of sheets needed is the ceiling of N/2.
+    // Using integer arithmetic, we can compute that as (N + 1) / 2.
+    int sheets = (N + 1) / 2;
+    cout << (N+1)/2 << endl;
 }
